a0bc8b57f1e894070774039f23cb2fc493555e5d
Ujef <ujef.kelawala123@gmail.com>
2024-10-14 00:17:51 +0530

    add initial redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux";
+import authService from "./Appwrite/auth";
+import { login, logout } from "./features/auth/authSlice";
+import { Outlet } from "react-router-dom";
+import Header from "./components/UI/Header/Header";
+import Footer from "./components/UI/Footer/Footer";
 
 const App = () => {
-  return (
-    <h1>Hello from app component</h1>
-  )
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }, [])
+
+  return !isLoading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
